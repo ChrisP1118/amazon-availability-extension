c2d60f5cba54e7937951275e690ec95cd88df414
Chris Wilson <cwilson@xaipete.net>
2020-04-08 22:15:44 -0500

    Added parallel load count

--- amazon-availability.js
+++ amazon-availability.js
@@ -2,48 +2,58 @@ let boxElements = [
     'dpFastTrackInsideBuyBox',
     'promiseBasedBadgeInsideBuyBox',
     'availabilityInsideBuyBox'
-]
+];
 
-console.log('Checking availability...');
+let parallelLoadCount = 3;
+
 let blocks = document.querySelectorAll('.s-result-list div[data-asin]');
-blocks.forEach((block, index) => {
-    if (index < 5) {
-        let anchor = block.querySelector('h2 a');
-        let name = anchor.querySelector('span').innerText;
-        let availabilityBlockContainer = block.querySelector('div.s-expand-height');
-        console.log(name + ': ' + anchor.href);
+console.log('Checking availability for ' + blocks.length + ' products...');
 
-        fetch(anchor.href, {
-            credentials: 'same-origin'
-        })
-        .then(response => {
-            if (response.status != 200) {
-                console.log('Error (status code ' + response.status + ') loading: ' + anchor.href);
-                return;
+let loadBlock = function (index) {
+    if (index >= blocks.length)
+        return;
+
+    let block = blocks[index];
+    let anchor = block.querySelector('h2 a');
+    let name = anchor.querySelector('span').innerText;
+    let availabilityBlockContainer = block.querySelector('div.s-expand-height');
+    console.log('Checking availability for "' + name + '" at: ' + anchor.href);
+
+    fetch(anchor.href, {
+        credentials: 'same-origin'
+    })
+    .then(response => {
+        if (response.status != 200) {
+            console.log('Error (status code ' + response.status + ') loading: ' + anchor.href);
+            return;
+        }
+
+        response.text().then(bodyText => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(bodyText, "text/html");
+            let buyBox = doc.getElementById('desktop_qualifiedBuyBox');
+            
+            if (buyBox) {
+                let availabilityBlock = document.createElement('div');
+                availabilityBlock.className = 'amazon-availability-box';
+
+                boxElements.forEach(boxElement => {
+                    let el = buyBox.querySelector('[data-feature-name="' + boxElement + '"]');
+                    if (el)
+                        availabilityBlock.appendChild(el);
+                })
+
+                availabilityBlockContainer.appendChild(availabilityBlock);
             }
-
-            response.text().then(bodyText => {
-                let parser = new DOMParser();
-                let doc = parser.parseFromString(bodyText, "text/html");
-                let buyBox = doc.getElementById('desktop_qualifiedBuyBox');
-                
-                if (buyBox) {
-                    let availabilityBlock = document.createElement('div');
-                    availabilityBlock.className = 'amazon-availability-box';
-
-                    boxElements.forEach(boxElement => {
-                        let el = buyBox.querySelector('[data-feature-name="' + boxElement + '"]');
-                        if (el)
-                            availabilityBlock.appendChild(el);
-                    })
-
-                    availabilityBlockContainer.appendChild(availabilityBlock);
-                }
-            })
         })
-        .catch(error => {
-            console.log(error);
-        });
-    }
-});
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(() => {
+        loadBlock(index + parallelLoadCount);
+    });
+}
 
+for (let i = 0; i < parallelLoadCount; ++i)
+    loadBlock(i);
