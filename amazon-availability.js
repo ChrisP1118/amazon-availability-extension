1b8538226388f7f83fcbe300003a0c26d768a676
Chris Wilson <cwilson@xaipete.net>
2020-04-08 23:11:58 -0500

    Lots of improvements

--- amazon-availability.js
+++ amazon-availability.js
@@ -1,22 +1,48 @@
 let boxElements = [
-    'dpFastTrackInsideBuyBox',
-    'promiseBasedBadgeInsideBuyBox',
-    'availabilityInsideBuyBox'
+    '[data-feature-name="dpFastTrackInsideBuyBox"]',
+    '[data-feature-name="promiseBasedBadgeInsideBuyBox"]',
+    '[data-feature-name="availabilityInsideBuyBox"]',
+    '[data-feature-name="businessOnlySelectionBuyBox"]',
+    '[data-feature-name="promiseBasedBadge"]',
+    '[data-feature-name="availability"]'
 ];
 
 let parallelLoadCount = 3;
 
-let blocks = document.querySelectorAll('.s-result-list div[data-asin]');
+let blocks = document.querySelectorAll('.s-result-list div[data-asin]:not([data-asin=""])');
 console.log('Checking availability for ' + blocks.length + ' products...');
+
+blocks.forEach(block => {
+    //let availabilityBlockContainer = block.querySelector('div.s-expand-height div.a-section');
+    let availabilityBlockContainer = block.querySelector('div.a-section');
+
+    if (!availabilityBlockContainer)
+        return;
+
+    availabilityBlockContainer = availabilityBlockContainer.parentElement;
+
+    let availabilityBlock = document.createElement('div');
+    availabilityBlock.className = 'amazon-availability-box';
+    availabilityBlock.innerText = 'Loading availability...';
+
+    availabilityBlockContainer.appendChild(availabilityBlock);
+})
 
 let loadBlock = function (index) {
     if (index >= blocks.length)
         return;
 
     let block = blocks[index];
+    let availabilityBlock = block.querySelector('div.amazon-availability-box');
     let anchor = block.querySelector('h2 a');
     let name = anchor.querySelector('span').innerText;
-    let availabilityBlockContainer = block.querySelector('div.s-expand-height');
+
+    if (!availabilityBlock) {
+        console.log('No availability block found for "' + name + '" (at: ' + anchor.href + ')');
+        loadBlock(index + parallelLoadCount);
+        return;
+    }
+
     console.log('Checking availability for "' + name + '" at: ' + anchor.href);
 
     fetch(anchor.href, {
@@ -25,27 +51,23 @@ let loadBlock = function (index) {
     .then(response => {
         if (response.status != 200) {
             console.log('Error (status code ' + response.status + ') loading: ' + anchor.href);
-            return;
-        }
-
-        response.text().then(bodyText => {
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(bodyText, "text/html");
-            let buyBox = doc.getElementById('desktop_qualifiedBuyBox');
-            
-            if (buyBox) {
-                let availabilityBlock = document.createElement('div');
-                availabilityBlock.className = 'amazon-availability-box';
+        } else {
+            response.text().then(bodyText => {
+                let parser = new DOMParser();
+                let doc = parser.parseFromString(bodyText, "text/html");
+                availabilityBlock.innerHTML = '';
 
                 boxElements.forEach(boxElement => {
-                    let el = buyBox.querySelector('[data-feature-name="' + boxElement + '"]');
+                    let el = doc.querySelector(boxElement);
                     if (el)
                         availabilityBlock.appendChild(el);
-                })
+                });
 
-                availabilityBlockContainer.appendChild(availabilityBlock);
-            }
-        })
+                if (availabilityBlock.innerHTML == '')
+                    availabilityBlock.innerHTML = 'No availability data found.';
+
+            })
+        }
     })
     .catch(error => {
         console.log(error);
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Testing
+
+Some search terms used for testing:
+* face mask
+* Pampers
+* Neutrogena
+* Galaxy S20+ screen protector
